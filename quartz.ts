1dc5d4caeee3a64dc7ed5862e81f5694f2aa2248
Siddhant Maji <siddhant.maji@gmail.com>
2026-06-04 23:41:23 -0500

    Quartz sync: Jun 4, 2026, 11:41 PM

--- quartz.ts
+++ quartz.ts
@@ -1,17 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
- 
-const config = await loadQuartzConfig({
-  theme: {
-    typography: {
-      header: {
-        name: "Schibsted Grotesk",
-        weights: [400, 700],
-        includeItalic: true,
-      },
-      body: "Source Sans Pro",
-      code: "IBM Plex Mono",
-    },
-  },
-})
+
+const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
--- quartz.ts.bak
+++ quartz.ts.bak
@@ -1,5 +1,17 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-
-const config = await loadQuartzConfig()
+ 
+const config = await loadQuartzConfig({
+  theme: {
+    typography: {
+      header: {
+        name: "Schibsted Grotesk",
+        weights: [400, 700],
+        includeItalic: true,
+      },
+      body: "Source Sans Pro",
+      code: "IBM Plex Mono",
+    },
+  },
+})
 export default config
 export const layout = await loadQuartzLayout()
